1004f4a7f21f9ec11af4d2dc7044e1e8f5185163
Lucas Gonzalez <lucas.acdc92@gmail.com>
2020-01-26 20:34:43 -0300

    the app deploy a list of task with bootstrap style

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component{
+    render(){
+        return(
+            
+          <nav className="navbar navbar-dark bg-dark">
+          <a  href="#" className="text-white">TASK
+           
+
+          </a>
+          
+        </nav>
+
+        )
+    }
+}
+
+export default Navigation;
